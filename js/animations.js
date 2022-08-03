22903e0a4bedb8841ee766a2d1bab9b5184138ce
hodorwho (LISE) <43373910+hodorwho@users.noreply.github.com>
2022-08-03 23:07:24 +0200

    jobber med js-filen, til toppen-knapp. Må enda lage en scroll ned og style knappen

--- js/animations.js
+++ js/animations.js
@@ -3,34 +3,11 @@
     target: '#navbar'
 })*/
 
-$("body").scrollspy({target: "navbar"});
+// $("body").scrollspy({target: "navbar"});
 
-/* animert scroll to top button fra https://anyday.inc/ideas/how-to-make-a-cool-animated-scroll-to-top-button/
-*/
-
-/*
-$(window).on('scroll', function(){
-    if (document.body.scrollTop > 20) {
-        $('#tilToppen').addClass('active')
-    }
-    else {
-        $('#tilToppen').removeClass('active')
-    }
-})
-
-$('#tilToppen').on('click',function() {
-    $(window.scrollTo({top:0, behavior: 'smooth'}))
-    $("html, body").animate({scrollTop: 0}, 500);
-})
-*/
-
-//const tilToppen = document.getElementById('tilToppen');
-//tilToppen.addEventListener("click", Toppen)
-
+/* animert scroll til toppen-knapp
+mangler enda at den fader inn og ut, pluss styling
+* */
 $("#tilToppen").on('click', function() {
     $(window.scrollTo({top:0, behavior: 'smooth'}))
 })
-
-//function Toppen() {
- //   $(window.scrollTo({top:0, behavior: 'smooth'}))
-//}
