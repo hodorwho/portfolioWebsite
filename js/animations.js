d4a74dc45398c6bbcc3184da986620cbf71b04b7
hodorwho (LISE) <43373910+hodorwho@users.noreply.github.com>
2022-08-09 19:27:58 +0200

    fikset scroll med navbar og justert H1, mangler enda accessability

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,33 @@
 // Scrollspy fra Bootstrap for sømløs scrolling/navigering fra navbar
 //$("body").scrollspy({target: "navbar"});
 
+/*
+$('html, body').animate({
+    scrollTop: $("#omMeg").offset().top + $("#nav").height() // minus the nav height
+}, 1000);
+*/
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '.navbar'
+})
+/*
+var navHeight = $(".navbar").height();
+alert(navHeight + "is navheight");
+$(".maincontainer").css({paddingTop: ($(".navbar").height())+'px'});
+*/
+// Scroll to id from nav items
+/*$(".navbar a").click(function () {
+    $("body,html").animate({
+        scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height() - 10
+    }, 1000)
+});
+*/
+/* scroll ned fra forsiden*/
+$(".arrowdiv").on('click', function () {
+    $('.omMeg')[0].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    $(window.scrollTo($('#omMeg').position()));
+});
+
 /* animert til toppen-knapp*/
 let tiltoppen = document.getElementById("tilToppen");
 
@@ -12,6 +39,10 @@ $("#tilToppen").on('click', function() {
     $(window.scrollTo({top:0, behavior: 'smooth'}))
 })
 
+function siFra() {
+    alert("knapp er trykket på nice");
+}
+
 /* fra YT-video: sticky navigation bar on scroll using vanilla javascript from online tutorials IKKE TESTET*/
 $(window.addEventListener("scroll", function() {
     let header = document.querySelector("nav");
@@ -19,12 +50,17 @@ $(window.addEventListener("scroll", function() {
     tiltoppen.classList.add("active");
 }))
 
-/*toggle for fagoversikt under utdanning */
-$(document).ready(function() {
+/*toggle for fagoversikt under utdanning
+$(document).ready(function() {*/
     $("#seFag").click(function() {
         $("#fagSeksjon").toggle("slow");
     });
-});
+/*});*/
+
+
+document.getElementById('seFag').onclick = function() {
+    alert("button was clicked");
+};
 
 const tl = gsap.timeline({defaults: {duration: 0.5, ease: "power1.in"}})
 
