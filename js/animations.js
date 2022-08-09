1b8048719df65f871e40d67ee4662cd6c8c19cc3
hodorwho (LISE) <43373910+hodorwho@users.noreply.github.com>
2022-08-10 01:13:01 +0200

    jobbet med rydding i kode, scalability og justering av margin, padding etc. Må enda skrive om tekst og se om det mangler aria-tags på noe.

--- js/animations.js
+++ js/animations.js
@@ -26,45 +26,37 @@ $(".maincontainer").css({paddingTop: ($(".navbar").height())+'px'});
 $(".arrowdiv").on('click', function () {
     $('.omMeg')[0].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     $(window.scrollTo($('#omMeg').position()));
+    console.log("done!");
 });
 
-/* animert til toppen-knapp*/
+/* animert "til toppen"-knapp*/
 let tiltoppen = document.getElementById("tilToppen");
 
-$("#tilToppen").on('click', function() {
-    /*vil at denne skal skjule knappen når man trykker på den, men det skjer bare
-    * når man er på toppen og trykker på den etter man har scrolla
-    tiltoppen.classList.remove("active"); */
-    $(tiltoppen.classList.remove("active"));
-    $(window.scrollTo({top:0, behavior: 'smooth'}))
-})
-
-function siFra() {
-    alert("knapp er trykket på nice");
-}
-
-/* fra YT-video: sticky navigation bar on scroll using vanilla javascript from online tutorials IKKE TESTET*/
+/* knappen dukker opp når man scroller*/
 $(window.addEventListener("scroll", function() {
     let header = document.querySelector("nav");
     header.classList.toggle("sticky", window.scrollY > 0);
     tiltoppen.classList.add("active");
 }))
 
-/*toggle for fagoversikt under utdanning
-$(document).ready(function() {*/
+/* sender deg til toppen*/
+$("#tilToppen").on('click', function() {
+    $(tiltoppen.classList.remove("active"));
+    $(window.scrollTo({top:0, behavior: 'smooth'}));
+})
+
+/* fra YT-video: sticky navigation bar on scroll using vanilla javascript from online tutorials IKKE TESTET*/
+
+
+/*toggle for fagoversikt under utdanning*/
     $("#seFag").click(function() {
         $("#fagSeksjon").toggle("slow");
     });
-/*});*/
 
-
-document.getElementById('seFag').onclick = function() {
-    alert("button was clicked");
-};
+/*animasjon av kopp under erfaring */
 
 const tl = gsap.timeline({defaults: {duration: 0.5, ease: "power1.in"}})
 
 //movement
 tl.fromTo('.cupOfCoffee', {y:1, rotation: '0deg'}, {y: -30, rotation:'20deg', yoyo: true, repeat: -1})
 tl.fromTo('#drops', {y:0, rotation: '0deg'}, {y: 30, rotation:'10deg', yoyo: true,  repeat: -1})
-//ease: "slow(0.1, 0.1, false)",
