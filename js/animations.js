bb000ef3c494258c786ef20a4ded9fdbbe6d7a9f
hodorwho (LISE) <43373910+hodorwho@users.noreply.github.com>
2022-08-10 23:03:23 +0200

    Nettsiden er lansert på lisemarilorentzen.com! Gjenstår litt småpirk og aria-tags, pluss omskriving av innhold.

--- js/animations.js
+++ js/animations.js
@@ -1,32 +1,26 @@
 // Scrollspy fra Bootstrap for sømløs scrolling/navigering fra navbar
 //$("body").scrollspy({target: "navbar"});
-
-/*
-$('html, body').animate({
-    scrollTop: $("#omMeg").offset().top + $("#nav").height() // minus the nav height
-}, 1000);
-*/
-
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '.navbar'
 })
-/*
-var navHeight = $(".navbar").height();
-alert(navHeight + "is navheight");
-$(".maincontainer").css({paddingTop: ($(".navbar").height())+'px'});
-*/
-// Scroll to id from nav items
-/*$(".navbar a").click(function () {
-    $("body,html").animate({
-        scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height() - 10
-    }, 1000)
-});
-*/
+
 /* scroll ned fra forsiden*/
 $(".arrowdiv").on('click', function () {
     $('.omMeg')[0].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     $(window.scrollTo($('#omMeg').position()));
     console.log("done!");
+});
+
+/* toggle-knapp for å skru av og på videobakgrunn
+* på store skjermer (er automatisk av på små)*/
+let video = document.getElementById("video");
+
+$('.togglebutton').on('click', function () {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
 });
 
 /* animert "til toppen"-knapp*/
@@ -45,16 +39,16 @@ $("#tilToppen").on('click', function() {
     $(window.scrollTo({top:0, behavior: 'smooth'}));
 })
 
-/* fra YT-video: sticky navigation bar on scroll using vanilla javascript from online tutorials IKKE TESTET*/
+/*toggle for fagoversikt under utdanning, knapp over og under innhold*/
+$("#seFag").click(function() {
+    $("#fagSeksjon").toggle("slow");
+});
 
+$("#lukkFag").click(function() {
+    $("#fagSeksjon").toggle();
+});
 
-/*toggle for fagoversikt under utdanning*/
-    $("#seFag").click(function() {
-        $("#fagSeksjon").toggle("slow");
-    });
-
-/*animasjon av kopp under erfaring */
-
+/*animasjon av kopp*/
 const tl = gsap.timeline({defaults: {duration: 0.5, ease: "power1.in"}})
 
 //movement
